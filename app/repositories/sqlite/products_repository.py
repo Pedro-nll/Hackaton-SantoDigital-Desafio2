f16608cb433692a69b4250431e71283b6cce55e8
Pedro-nll <pedronll@outlook.com>
2024-07-12 11:44:24 -0300

    Product repo

--- app/repositories/sqlite/products_repository.py
+++ app/repositories/sqlite/products_repository.py
@@ -10,28 +10,46 @@ class SQLiteProductsRepository(ProductsDatabaseInterface):
 
     def create_tables(self):
         with self.connection:
+            self.connection.execute('PRAGMA foreign_keys = ON;')
             self.connection.execute(
                 '''CREATE TABLE IF NOT EXISTS products (
-                    product_id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    description TEXT,
-                    price REAL
+                    product_key INTEGER PRIMARY KEY,
+                    product_price REAL,
+                    product_subcategory_key INTEGER,
+                    product_sku TEXT,
+                    product_name TEXT,
+                    model_name TEXT,
+                    product_description TEXT,
+                    product_color TEXT,
+                    product_size TEXT,
+                    product_style TEXT,
+                    product_cost REAL,
+                    FOREIGN KEY (product_subcategory_key) REFERENCES product_subcategories(product_subcategory_key)
                 )'''
             )
 
     def add_product(self, product: Product):
-        sql = 'INSERT INTO products (product_id, name, description, price) VALUES (?, ?, ?, ?)'
-        params = (product.product_id, product.name, product.description, product.price)
+        sql = '''INSERT INTO products (product_key, product_price, product_subcategory_key, product_sku, product_name,
+                                       model_name, product_description, product_color, product_size, product_style, product_cost) 
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+        params = (product.productKey, product.productPrice, product.productSubcategoryKey, product.productSKU, 
+                  product.productName, product.modelName, product.productDescription, product.productColor, 
+                  product.productSize, product.productStyle, product.productCost)
         self.execute_transaction(sql, params)
 
-    def delete_product(self, product_id: int):
-        sql = 'DELETE FROM products WHERE product_id = ?'
-        params = (product_id,)
+    def delete_product(self, product_key: int):
+        sql = 'DELETE FROM products WHERE product_key = ?'
+        params = (product_key,)
         self.execute_transaction(sql, params)
 
     def update_product(self, product: Product):
-        sql = 'UPDATE products SET name = ?, description = ?, price = ? WHERE product_id = ?'
-        params = (product.name, product.description, product.price, product.product_id)
+        sql = '''UPDATE products SET product_price = ?, product_subcategory_key = ?, product_sku = ?, product_name = ?, 
+                                    model_name = ?, product_description = ?, product_color = ?, product_size = ?, 
+                                    product_style = ?, product_cost = ? 
+                 WHERE product_key = ?'''
+        params = (product.productPrice, product.productSubcategoryKey, product.productSKU, product.productName, 
+                  product.modelName, product.productDescription, product.productColor, product.productSize, 
+                  product.productStyle, product.productCost, product.productKey)
         self.execute_transaction(sql, params)
 
     def execute_transaction(self, sql: str, params: tuple):
@@ -45,15 +63,15 @@ class SQLiteProductsRepository(ProductsDatabaseInterface):
         else:
             self.connection.commit()
 
-    def get_product(self, product_id: int) -> Product:
+    def get_product(self, product_key: int) -> Product:
         cursor = self.connection.cursor()
-        cursor.execute('SELECT product_id, name, description, price FROM products WHERE product_id = ?', (product_id,))
+        cursor.execute('SELECT * FROM products WHERE product_key = ?', (product_key,))
         row = cursor.fetchone()
         return Product(*row) if row else None
 
     def get_all_products(self) -> list[Product]:
         cursor = self.connection.cursor()
-        cursor.execute('SELECT product_id, name, description, price FROM products')
+        cursor.execute('SELECT * FROM products')
         rows = cursor.fetchall()
         return [Product(*row) for row in rows]
 
